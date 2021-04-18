b32eaf638866923997e9ec0748535a9032f2cafd
kyson zhu <zjh171@qq.com>
2021-04-19 07:41:43 +0800

    [ADD] gojitomvvm

---
+++ GJTGojitoMvvmKit/Classes/GJTBaseScrollViewController.h
@@ -0,0 +1,19 @@
+//
+//  GJTBaseScrollViewController.h
+//  GJTMvvmKit
+//
+//  Created by kyson on 2021/2/27.
+//
+
+#import <GJTMvvmKit/GJTMvvmKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GJTBaseScrollViewController : GJTScrollViewController
+
+
+@property(nonatomic, strong,readonly) UIScrollView *scrollView;
+
+@end
+
+NS_ASSUME_NONNULL_END
